05d9b50dcf5f73ae166b401ab4bc2cce0094d152
MUHAMMET KOMUR <muhammet075@icloud.com>
2023-05-18 14:57:24 +0200

    making footer responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,48 +22,50 @@ function Footer() {
         
         <section>
             <div>
-                <Link href="/"><Image src={Logo} alt="Logo of Tickety"/></Link>
-                <ul>
-                    <li><Link href="/"><Image src={instagramIco} alt="Instagram Logo"/></Link></li>
-                    <li><Link href="/"><Image src={linkedinIco} alt="Linkedin Logo"/></Link></li>
-                    <li><Link href="/"><Image src={facebookIco} alt="Facebook Logo"/></Link></li>
-                    <li><Link href="/"><Image src={twitterIco} alt="Twitter Logo"/></Link></li>
-                </ul>
-            </div>
+                <div>
+                    <Link href="/"><Image src={Logo} alt="Logo of Tickety"/></Link>
+                    <ul>
+                        <li><Link href="/"><Image src={instagramIco} alt="Instagram Logo"/></Link></li>
+                        <li><Link href="/"><Image src={linkedinIco} alt="Linkedin Logo"/></Link></li>
+                        <li><Link href="/"><Image src={facebookIco} alt="Facebook Logo"/></Link></li>
+                        <li><Link href="/"><Image src={twitterIco} alt="Twitter Logo"/></Link></li>
+                    </ul>
+                </div>
 
-            <div>
-                <h3>Our Company</h3>
-                <ul>
-                    <li><Link href="/">About</Link></li>
-                    <li><Link href="/">Who We Are</Link></li>
-                    <li><Link href="/">Blog</Link></li>
-                    <li><Link href="/">Privacy Policy</Link></li>
-                    <li><Link href="/">Careers</Link></li>
-                    <li><Link href="/">Terms</Link></li>
-                </ul>
-            </div>
+                <div>
+                    <h3>Our Company</h3>
+                    <ul>
+                        <li><Link href="/">About</Link></li>
+                        <li><Link href="/">Who We Are</Link></li>
+                        <li><Link href="/">Blog</Link></li>
+                        <li><Link href="/">Privacy Policy</Link></li>
+                        <li><Link href="/">Careers</Link></li>
+                        <li><Link href="/">Terms</Link></li>
+                    </ul>
+                </div>
 
-            <div>
-                <h3>Browser By</h3>
-                <ul>
-                    <li><Link href="/">All Events</Link></li>
-                    <li><Link href="/">NFL</Link></li>
-                    <li><Link href="/">NBA</Link></li>
-                    <li><Link href="/">MLB</Link></li>
-                    <li><Link href="/">F1</Link></li>
-                    <li><Link href="/">FE</Link></li>
-                </ul>
-            </div>
+                <div>
+                    <h3>Browser By</h3>
+                    <ul>
+                        <li><Link href="/">All Events</Link></li>
+                        <li><Link href="/">NFL</Link></li>
+                        <li><Link href="/">NBA</Link></li>
+                        <li><Link href="/">MLB</Link></li>
+                        <li><Link href="/">F1</Link></li>
+                        <li><Link href="/">FE</Link></li>
+                    </ul>
+                </div>
 
-            <div>
-                <h3>Helpful Links</h3>
-                <ul>
-                    <li><Link href="/">Help/FAQ</Link></li>
-                    <li><Link href="/">My Account</Link></li>
-                    <li><Link href="/">Contact Us</Link></li>
-                    <li><Link href="/">Gift Cards</Link></li>
-                    <li><Link href="/">Careers</Link></li>
-                </ul>
+                <div>
+                    <h3>Helpful Links</h3>
+                    <ul>
+                        <li><Link href="/">Help/FAQ</Link></li>
+                        <li><Link href="/">My Account</Link></li>
+                        <li><Link href="/">Contact Us</Link></li>
+                        <li><Link href="/">Gift Cards</Link></li>
+                        <li><Link href="/">Careers</Link></li>
+                    </ul>
+                </div>
             </div>
         </section>
 
