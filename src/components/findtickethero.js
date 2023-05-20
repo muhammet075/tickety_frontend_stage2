7a18370520b65e17b1bf4cadb513f3e520d0254f
MUHAMMET KOMUR <muhammet075@icloud.com>
2023-05-20 05:39:32 +0200

    adding navigation on the events

--- src/components/findtickethero.js
+++ src/components/findtickethero.js
@@ -7,8 +7,12 @@ import linkedinIco from "../assets/icons/linkedin.svg";
 import facebookIco from "../assets/icons/facebook.svg";
 import twitterIco from "../assets/icons/twitter.svg";
 import searchIco from "../assets/icons/search.svg";
+import leftIco from "../assets/icons/white-left.svg";
+import rightIco from "../assets/icons/white-right.svg";
 
 function Findtickethero() {
+  const [sliderIndex, setSliderIndex] = useState(0);
+
   useEffect(() => {
     const storedEvents = JSON.parse(sessionStorage.getItem("events"));
 
@@ -58,9 +62,11 @@ function Findtickethero() {
     createTickets();
   }
 
-  function createTickets(){
+  function createTickets() {
     setTimeout(function () {
-      document.querySelector(".resetdatasection").classList.remove("displaynone");
+      document
+        .querySelector(".resetdatasection")
+        .classList.remove("displaynone");
       document.querySelector(".filterdatasection").classList.add("displaynone");
       document.querySelector(".searchcontainer").classList.add("displaynone");
       document.querySelector(".headercontent").classList.add("resetheader");
@@ -69,85 +75,97 @@ function Findtickethero() {
       let html = "";
 
       for (let i = 0; i < Math.min(storedEvents.length, 7); i++) {
+        let randomSection;
+        const teamName = sessionStorage.getItem("teamname");
 
-      let randomSection;
-      const teamName = sessionStorage.getItem("teamname");
-
-      if (teamName === "baltimoreravens"){ 
-        const randomNumber = Math.floor(Math.random() * 3);
-        if (randomNumber === 0) {
-          randomSection = String(Math.floor(Math.random() * 54) + 500);
-        } else if (randomNumber === 1) {
-          randomSection = String(Math.floor(Math.random() * 54) + 200);
-        } else {
-          randomSection = String(Math.floor(Math.random() * 54) + 100);
+        if (teamName === "baltimoreravens") {
+          const randomNumber = Math.floor(Math.random() * 3);
+          if (randomNumber === 0) {
+            randomSection = String(Math.floor(Math.random() * 54) + 500);
+          } else if (randomNumber === 1) {
+            randomSection = String(Math.floor(Math.random() * 54) + 200);
+          } else {
+            randomSection = String(Math.floor(Math.random() * 54) + 100);
+          }
+        } else if (teamName === "atlantafalcons") {
+          const randomNumber = Math.floor(Math.random() * 3);
+          if (randomNumber === 0) {
+            randomSection = String(Math.floor(Math.random() * 50) + 301);
+          } else if (randomNumber === 1) {
+            randomSection = String(Math.floor(Math.random() * 47) + 201);
+          } else {
+            randomSection = String(Math.floor(Math.random() * 36) + 101);
+          }
+        } else if (teamName === "arizonacardinals") {
+          const randomNumber = Math.floor(Math.random() * 2);
+          if (randomNumber === 0) {
+            randomSection = String(Math.floor(Math.random() * 48) + 201);
+          } else {
+            randomSection = String(Math.floor(Math.random() * 26) + 119);
+          }
         }
 
-      } else if (teamName === "atlantafalcons"){
-        const randomNumber = Math.floor(Math.random() * 3);
-        if (randomNumber === 0) {
-          randomSection = String(Math.floor(Math.random() * 50) + 301);
-        } else if (randomNumber === 1) {
-          randomSection = String(Math.floor(Math.random() * 47) + 201);
-        } else {
-          randomSection = String(Math.floor(Math.random() * 36) + 101);
+        const priceRangeElement = document.querySelector(".pricerange");
+        const selectedPriceRange = parseInt(priceRangeElement.value, 10);
+        const maxPrice = selectedPriceRange;
+        const randomPrice =
+          Math.floor(Math.random() * (maxPrice - 175 + 1)) + 175;
+        const randomRow = String(Math.floor(Math.random() * 20) + 1).padStart(
+          2,
+          "0"
+        );
+        const randomSeats = String(Math.floor(Math.random() * 18) + 1).padStart(
+          2,
+          "0"
+        );
+        const randomSeatsSecond = String(Number(randomSeats) + 1).padStart(
+          2,
+          "0"
+        );
+        let priceRatio = randomPrice / parseInt(randomSection);
+
+        const date = new Date(storedEvents[i].date);
+        const month = date
+          .toLocaleString("default", { month: "short" })
+          .replace(/^(.)/, (c) => c.toUpperCase());
+        const day = date.getDate();
+        const dayOfWeek = date.toLocaleString("en-US", { weekday: "short" });
+        const formattedDate = `${month} ${day} - ${dayOfWeek}`;
+        const timeString = storedEvents[i].time;
+        const timeParts = timeString.split(":");
+        const hours = parseInt(timeParts[0], 10);
+        const minutes = parseInt(timeParts[1], 10);
+        const time = new Date();
+        time.setHours(hours);
+        time.setMinutes(minutes);
+        const formattedTime = time.toLocaleString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        });
+
+        const eventName = storedEvents[i].event_name.replace(/\./g, "");
+        const separators = [" vs ", " Vs ", " v "];
+        let separator = null;
+
+        for (let j = 0; j < separators.length; j++) {
+          if (eventName.includes(separators[j])) {
+            separator = separators[j];
+            break;
+          }
         }
 
-      } else if (teamName === "arizonacardinals"){
-        const randomNumber = Math.floor(Math.random() * 2);
-        if (randomNumber === 0) {
-          randomSection = String(Math.floor(Math.random() * 48) + 201);
-        } else {
-          randomSection = String(Math.floor(Math.random() * 26) + 119);
+        let flippedEventName = eventName;
+        if (separator) {
+          const teams = eventName.split(separator);
+          const homeTeam = teams[0].trim();
+          const awayTeam = teams[1].trim();
+          flippedEventName = `${awayTeam} ${separator.trim()} ${homeTeam}`;
         }
-      }
 
-      const priceRangeElement = document.querySelector(".pricerange");
-      const selectedPriceRange = parseInt(priceRangeElement.value, 10)
-      const maxPrice = selectedPriceRange;
-      const randomPrice = Math.floor(Math.random() * (maxPrice - 175 + 1)) + 175;
-      const randomRow = String(Math.floor(Math.random() * 20) + 1).padStart(2, "0");
-      const randomSeats = String(Math.floor(Math.random() * 18) + 1).padStart(2, "0");
-      const randomSeatsSecond = String(Number(randomSeats) + 1).padStart(2, "0");
-      let priceRatio = randomPrice / parseInt(randomSection); 
+        console.log(flippedEventName);
 
-      const date = new Date(storedEvents[i].date);
-      const month = date.toLocaleString('default', { month: 'short' }).replace(/^(.)/, c => c.toUpperCase());
-      const day = date.getDate();
-      const dayOfWeek = date.toLocaleString('en-US', { weekday: 'short' });
-      const formattedDate = `${month} ${day} - ${dayOfWeek}`;
-      const timeString = storedEvents[i].time;
-      const timeParts = timeString.split(":");
-      const hours = parseInt(timeParts[0], 10);
-      const minutes = parseInt(timeParts[1], 10);
-      const time = new Date();
-      time.setHours(hours);
-      time.setMinutes(minutes);
-      const formattedTime = time.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-
-      const eventName = storedEvents[i].event_name.replace(/\./g, "");
-const separators = [" vs ", " Vs ", " v "];
-let separator = null;
-
-for (let j = 0; j < separators.length; j++) {
-  if (eventName.includes(separators[j])) {
-    separator = separators[j];
-    break;
-  }
-}
-
-let flippedEventName = eventName;
-if (separator) {
-  const teams = eventName.split(separator);
-  const homeTeam = teams[0].trim();
-  const awayTeam = teams[1].trim();
-  flippedEventName = `${awayTeam} ${separator.trim()} ${homeTeam}`;
-}
-
-      console.log(flippedEventName);
-
-
-  html += `
+        html += `
           <div class="card">
             <div>
               <section>
@@ -160,64 +178,116 @@ if (separator) {
                 <p>Section: <span class="sectionnumber">${randomSection}</span></p>
                 <p>Row: ${randomRow}</span></p>
                 <p>Seat: ${randomSeats} and ${randomSeatsSecond}</p>
+              </section>
+
+              <section>
                 <p>$ ${randomPrice}.00 </p>
                 <button type="button" onclick="alert('test')">Buy</button>
               </section>
             </div>
           </div>
         `;
-  html += "";
-  document.querySelector(".ticketresults").classList.remove("displaynone");
-  document.querySelector(".herosocials").classList.add("displaynone");
-  document.querySelector(".ticketshero").classList.add("nobackgroundimage");
-  document.querySelector(".cards-box").innerHTML = html;
-  window.scrollTo({ top: 0, behavior: "smooth" });
- }
+        html += "";
+        document
+          .querySelector(".ticketresults")
+          .classList.remove("displaynone");
+        document.querySelector(".herosocials").classList.add("displaynone");
+        document
+          .querySelector(".ticketshero")
+          .classList.add("nobackgroundimage");
+        document.querySelector(".cards-box").innerHTML = html;
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }
       loopTickets();
       putSectionBackgrounds();
+      updateCardTransforms(sliderIndex);
     }, 500);
   }
 
-  function putSectionBackgrounds(){
+  function putSectionBackgrounds() {
     let sectionNumbers = document.querySelectorAll(".sectionnumber");
     let rowNumbers = document.querySelectorAll(".rownumber");
     const teamName = sessionStorage.getItem("teamname");
     for (let i = 0; i < sectionNumbers.length; i++) {
-      let backgroundUrl = 'url("/nfl-seat-backgrounds/' + teamName + '/section_section-' +  sectionNumbers[i].innerHTML + '.jpg")';
-      sectionNumbers[i].parentElement.parentElement.parentElement.parentElement.style.backgroundImage = backgroundUrl;
+      let backgroundUrl =
+        'url("/nfl-seat-backgrounds/' +
+        teamName +
+        "/section_section-" +
+        sectionNumbers[i].innerHTML +
+        '.jpg")';
+      sectionNumbers[
+        i
+      ].parentElement.parentElement.parentElement.parentElement.style.backgroundImage =
+        backgroundUrl;
     }
-  };
-
-  // function resetData(){
-  //   sessionStorage.removeItem("events");
-  //   location.reload();
-  // }
-
-  function loopTickets(){
-      let sliderImagesBox = document.querySelectorAll(".cards-box");
-      sliderImagesBox.forEach((el) => {
-        let imageNodes = el.querySelectorAll(".card:not(.hide)");
-        let arrIndexes = []; 
-        (() => {
-          let start = 0;
-          while (imageNodes.length > start) {
-            arrIndexes.push(start++);
-          }
-        })();
-
-        let setIndex = (arr) => {
-          for (let i = 0; i < imageNodes.length; i++) {
-            imageNodes[i].dataset.slide = arr[i];
-          }
-        };
-        el.addEventListener("click", () => {
-          arrIndexes.unshift(arrIndexes.pop());
-          setIndex(arrIndexes);
-        });
-        setIndex(arrIndexes);
-      });
   }
 
+  function loopTickets() {
+    let sliderImagesBox = document.querySelectorAll(".cards-box");
+    sliderImagesBox.forEach((el) => {
+      let imageNodes = el.querySelectorAll(".card:not(.hide)");
+      let arrIndexes = [];
+      (() => {
+        let start = 0;
+        while (imageNodes.length > start) {
+          arrIndexes.push(start++);
+        }
+      })();
+
+      let setIndex = (arr) => {
+        for (let i = 0; i < imageNodes.length; i++) {
+          imageNodes[i].dataset.slide = arr[i];
+        }
+      };
+
+      el.addEventListener("click", () => {
+        arrIndexes.unshift(arrIndexes.pop());
+        setIndex(arrIndexes);
+      });
+
+      setIndex(arrIndexes);
+    });
+  }
+
+
+  
+
+    function prevTickets() {
+      const cards = document.querySelectorAll(".cards-box .card");
+      const currentIndex = Array.from(cards).findIndex(
+        (card) => card.getAttribute("data-slide") === "0"
+      );
+      const prevIndex = currentIndex > 0 ? currentIndex - 1 : cards.length - 1;
+
+      cards.forEach((card, i) => {
+        const slideValue = Math.abs(i - prevIndex);
+        card.setAttribute("data-slide", slideValue);
+      });
+    }
+
+    function nextTickets() {
+      const cards = document.querySelectorAll(".cards-box .card");
+      const currentIndex = Array.from(cards).findIndex(
+        (card) => card.getAttribute("data-slide") === "0"
+      );
+      const nextIndex = (currentIndex + 1) % cards.length;
+
+      cards.forEach((card, i) => {
+        const slideValue = Math.abs(i - nextIndex);
+        card.setAttribute("data-slide", slideValue);
+      });
+    }
+
+    function updateCardTransforms(index) {
+      const cards = document.querySelectorAll(".cards-box .card");
+
+      cards.forEach((card, i) => {
+        const slideValue = Math.abs(i - index);
+        card.setAttribute("data-slide", slideValue);
+      });
+    }
+
+  
 
   return (
     <div className={styles.findtickethero + " ticketshero"}>
@@ -237,9 +307,14 @@ if (separator) {
 
       <section className='ticketresults displaynone'>
         <div class='cards-box'></div>
+
+        <div>
+          <button onClick={nextTickets} className='next-button'><Image src={leftIco} alt="Left icon"/></button>
+          <button onClick={prevTickets} className='prev-button'><Image src={rightIco} alt="Right icon"/></button>
+        </div>
       </section>
 
-      <section className="herosocials">
+      <section className='herosocials'>
         <ul>
           <li>
             <Link href='#'>
