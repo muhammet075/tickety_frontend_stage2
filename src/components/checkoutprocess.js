a693d324c4b41d51229251548aab3f5ac567b068
MUHAMMET KOMUR <muhammet075@icloud.com>
2023-05-21 05:09:04 +0200

    updating basket page

--- src/components/checkoutprocess.js
+++ src/components/checkoutprocess.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import styles from "../styles/checkoutprocess.module.css";
 import applePayIco from "../assets/icons/applepaywhite.png";
 import basketIco from "../assets/icons/basket.svg";
+import deleteIco from "../assets/icons/delete.png";
 
 function Checkoutprocess() {
   useEffect(() => {
@@ -12,40 +13,39 @@ function Checkoutprocess() {
 
     if (savedEventJSON !== null) {
         document.querySelector(".loadingstate").classList.remove("displaynone");
-
         setTimeout(function () {
-        document.querySelector(".loadingstate").classList.add("displaynone");
-        document.querySelector(".resetdatasection").classList.remove("displaynone");
-        document.querySelector(".showprice").classList.remove("displaynone");
-        document.querySelector(".filterdatasection").classList.add("displaynone");
-        document.querySelector(".resetdatasection").lastElementChild.lastElementChild.innerHTML = "Remove Ticket";
-        document.querySelector(".resetdatasection").lastElementChild.firstElementChild.src = "/img/trash.svg";
-        document.querySelector(".baskettext").innerHTML ="Select the event from the options below that you would like to purchase using Apple Pay.";
+            document.querySelector(".loadingstate").classList.add("displaynone");
+            document.querySelector(".deletebtn").classList.remove("displaynone");
+            document.querySelector(".resetdatasection").classList.remove("displaynone");
+            document.querySelector(".showprice").classList.remove("displaynone");
+            document.querySelector(".filterdatasection").classList.add("displaynone");
+            document.querySelector(".resetdatasection").lastElementChild.lastElementChild.innerHTML = "Remove Ticket";
+            document.querySelector(".resetdatasection").lastElementChild.firstElementChild.src = "/img/trash.svg";
+            document.querySelector(".baskettext").innerHTML ="Select the event from the options below that you would like to purchase using Apple Pay.";
 
-        const getEvent = sessionStorage.getItem("savedEvent");
-        const showGetEvent = JSON.parse(getEvent);
-        const date = showGetEvent.date;
-        const eventname = showGetEvent.eventname;
-        const location = showGetEvent.location;
-        const row = showGetEvent.row;
-        const seat = showGetEvent.seat;
-        const section = showGetEvent.section;
-        const imgurl = showGetEvent.imgurl;
-        var amount = showGetEvent.price;
-        var number = parseFloat(amount.replace("$ ", ""));
-        var multiplied = number * 2;
-        const price = "$ " + multiplied.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            const getEvent = sessionStorage.getItem("savedEvent");
+            const showGetEvent = JSON.parse(getEvent);
+            const date = showGetEvent.date;
+            const eventname = showGetEvent.eventname;
+            const location = showGetEvent.location;
+            const row = showGetEvent.row;
+            const seat = showGetEvent.seat;
+            const section = showGetEvent.section;
+            const imgurl = showGetEvent.imgurl;
+            var amount = showGetEvent.price;
+            var number = parseFloat(amount.replace("$ ", ""));
+            var multiplied = number * 2;
+            const price = "$ " + multiplied.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
-
-        document.querySelector(".showdate").innerHTML = date;
-        document.querySelector(".showevent").innerHTML = eventname;
-        document.querySelector(".showlocation").innerHTML = location;
-        document.querySelector(".showprice").innerHTML = price;
-        document.querySelector(".showrow").innerHTML = row;
-        document.querySelector(".showseat").innerHTML = seat;
-        document.querySelector(".showsection").innerHTML = section;
-        document.querySelector(".backgroundeventticket").style.backgroundImage = imgurl;
-        document.querySelector(".resetbtn").classList.remove("displaynone");
+            document.querySelector(".showdate").innerHTML = date;
+            document.querySelector(".showevent").innerHTML = eventname;
+            document.querySelector(".showlocation").innerHTML = location;
+            document.querySelector(".showprice").innerHTML = price;
+            document.querySelector(".showrow").innerHTML = row;
+            document.querySelector(".showseat").innerHTML = seat;
+            document.querySelector(".showsection").innerHTML = section;
+            document.querySelector(".backgroundeventticket").style.backgroundImage = imgurl;
+            document.querySelector(".resetbtn").classList.remove("displaynone");
         }, 500);
     } else {
         document.querySelector(".baskettext").innerHTML = "There are no events in your shopping cart. Please return to the home page to add an event.";
@@ -56,6 +56,11 @@ function Checkoutprocess() {
         document.querySelector(".resetbtn").classList.add("displaynone");
     }
   });
+
+  function deleteEvent(){
+    sessionStorage.removeItem("savedEvent");
+    window.location.reload();
+  }
 
   function payFunction(){
     alert("Apple Pay process")
@@ -83,6 +88,7 @@ function Checkoutprocess() {
             <p>Row: <span className='showrow'></span></p>
             <p>Seats: <span  className='showseat'></span></p>
             <p>Section: <span className='showsection'></span></p>
+            <button onClick={deleteEvent} className="deletebtn displaynone"><Image src={deleteIco} alt="Delte Icon"/></button>
             <button onClick={payFunction}><Image src={applePayIco} alt="Apple Pay Logo"/></button>
           </div>
         </div>
